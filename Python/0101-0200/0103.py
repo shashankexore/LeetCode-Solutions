6a0a2f3090c55913ed485bbe7861613f2377a3da
Shashank Singh <156345859+shashankexore@users.noreply.github.com>
2025-02-04 00:20:34 +0530

    0103.py

---
+++ Python/0101-0200/0103.py
@@ -0,0 +1,20 @@
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        ans = []
+        if root is None:
+            return ans
+        q = deque([root])
+        ans = []
+        left = 1
+        while q:
+            t = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                t.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            ans.append(t if left else t[::-1])
+            left ^= 1
+        return ans
