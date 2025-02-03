7bad15ede38628540220b67357441c8750cfdee0
Shashank Singh <156345859+shashankexore@users.noreply.github.com>
2025-02-04 00:20:57 +0530

    0104.py

---
+++ Python/0101-0200/0104.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        l, r = self.maxDepth(root.left), self.maxDepth(root.right)
+        return 1 + max(l, r)
