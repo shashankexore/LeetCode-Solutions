d7d6059483f733d14280e224def990673b526a4e
Shashank Singh <156345859+shashankexore@users.noreply.github.com>
2025-02-03 21:48:40 +0530

    0002.py

--- Python/0001-0100/0002.py
+++ Python/0001-0100/0002.py
@@ -12,3 +12,5 @@ class Solution:
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
         return dummy.next
+
+#https://leetcode.com/problems/add-two-numbers/description/
