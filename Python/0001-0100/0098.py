80cd963f974c8044f63ddce7bf6468f15374bd0c
Shashank Singh <156345859+shashankexore@users.noreply.github.com>
2025-02-03 19:10:49 +0530

    0098.py

---
+++ Python/0001-0100/0098.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def dfs(root: Optional[TreeNode]) -> bool:
+            if root is None:
+                return True
+            if not dfs(root.left):
+                return False
+            nonlocal prev
+            if prev >= root.val:
+                return False
+            prev = root.val
+            return dfs(root.right)
+
+        prev = -inf
+        return dfs(root)
