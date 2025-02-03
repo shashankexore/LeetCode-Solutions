3c41cde49b6ee27761aa9a64dbdc41c5a60552ca
Shashank Singh <156345859+shashankexore@users.noreply.github.com>
2025-02-03 21:49:29 +0530

    0004.py

--- Python/0001-0100/0004.py
+++ Python/0001-0100/0004.py
@@ -16,3 +16,5 @@ class Solution:
         a = f(0, 0, (m + n + 1) // 2)
         b = f(0, 0, (m + n + 2) // 2)
         return (a + b) / 2
+
+#https://leetcode.com/problems/median-of-two-sorted-arrays/
