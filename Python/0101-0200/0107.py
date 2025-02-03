70d3386da0905c5c7b099bff3db8b16eca832a82
Shashank Singh <156345859+shashankexore@users.noreply.github.com>
2025-02-04 00:21:58 +0530

    0107.py

---
+++ Python/0101-0200/0107.py
@@ -0,0 +1,17 @@
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        ans = []
+        if root is None:
+            return ans
+        q = deque([root])
+        while q:
+            t = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                t.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            ans.append(t)
+        return ans[::-1]
