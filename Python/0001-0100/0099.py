a481c715e86c045f297b3313ad1154f8f9eb5538
Shashank Singh <156345859+shashankexore@users.noreply.github.com>
2025-02-03 19:11:27 +0530

    0099.py

---
+++ Python/0001-0100/0099.py
@@ -0,0 +1,21 @@
+class Solution:
+    def recoverTree(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+
+        def dfs(root):
+            if root is None:
+                return
+            nonlocal prev, first, second
+            dfs(root.left)
+            if prev and prev.val > root.val:
+                if first is None:
+                    first = prev
+                second = root
+            prev = root
+            dfs(root.right)
+
+        prev = first = second = None
+        dfs(root)
+        first.val, second.val = second.val, first.val
