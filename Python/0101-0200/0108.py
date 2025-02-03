13a49b942fa6c45b3e1967a5e7ff4d6e1abe0a9c
Shashank Singh <156345859+shashankexore@users.noreply.github.com>
2025-02-04 00:22:16 +0530

    0108.py

---
+++ Python/0101-0200/0108.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        def dfs(l: int, r: int) -> Optional[TreeNode]:
+            if l > r:
+                return None
+            mid = (l + r) >> 1
+            return TreeNode(nums[mid], dfs(l, mid - 1), dfs(mid + 1, r))
+
+        return dfs(0, len(nums) - 1)
