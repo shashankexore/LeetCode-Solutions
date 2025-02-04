d8aa511247dc84e2480584e44516d0b2e857edc4
Shashank Singh <156345859+shashankexore@users.noreply.github.com>
2025-02-05 03:49:05 +0530

    0114.py

---
+++ Python/0101-0200/0114.py
@@ -0,0 +1,14 @@
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        while root:
+            if root.left:
+                pre = root.left
+                while pre.right:
+                    pre = pre.right
+                pre.right = root.right
+                root.right = root.left
+                root.left = None
+            root = root.right
