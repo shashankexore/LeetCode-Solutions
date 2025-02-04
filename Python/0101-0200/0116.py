0b3a21c13fc13e20eeb203817b6077c6b782631b
Shashank Singh <156345859+shashankexore@users.noreply.github.com>
2025-02-05 03:50:10 +0530

    0116.py

---
+++ Python/0101-0200/0116.py
@@ -0,0 +1,17 @@
+class Solution:
+    def connect(self, root: "Optional[Node]") -> "Optional[Node]":
+        if root is None:
+            return root
+        q = deque([root])
+        while q:
+            p = None
+            for _ in range(len(q)):
+                node = q.popleft()
+                if p:
+                    p.next = node
+                p = node
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+        return root
