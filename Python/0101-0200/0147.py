71ccf6245367eb36638fc53eb997aa291bafdc1c
Shashank Singh <156345859+shashankexore@users.noreply.github.com>
2025-02-08 03:01:56 +0530

    0147.py

---
+++ Python/0101-0200/0147.py
@@ -0,0 +1,19 @@
+class Solution:
+    def insertionSortList(self, head: ListNode) -> ListNode:
+        if head is None or head.next is None:
+            return head
+        dummy = ListNode(head.val, head)
+        pre, cur = dummy, head
+        while cur:
+            if pre.val <= cur.val:
+                pre, cur = cur, cur.next
+                continue
+            p = dummy
+            while p.next.val <= cur.val:
+                p = p.next
+            t = cur.next
+            cur.next = p.next
+            p.next = cur
+            pre.next = t
+            cur = t
+        return dummy.next
