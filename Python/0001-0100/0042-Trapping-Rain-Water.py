9e866aaeb1adff924248c93d896a675f93018cb2
Shashank Singh <156345859+shashankexore@users.noreply.github.com>
2025-02-08 19:38:21 +0530

    Trapping Rain Water

--- Python/0001-0100/0042-Trapping-Rain-Water.py
+++ Python/0001-0100/0042-Trapping-Rain-Water.py
@@ -7,3 +7,5 @@ class Solution:
             left[i] = max(left[i - 1], height[i])
             right[n - i - 1] = max(right[n - i], height[n - i - 1])
         return sum(min(l, r) - h for l, r, h in zip(left, right, height))
+
+#https://leetcode.com/problems/trapping-rain-water/
