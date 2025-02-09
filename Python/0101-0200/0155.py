b5d3c389e9e15c4f0373a5b992abfd6d1215c4a4
Shashank Singh <156345859+shashankexore@users.noreply.github.com>
2025-02-09 06:00:12 +0530

    0155.py

---
+++ Python/0101-0200/0155.py
@@ -0,0 +1,18 @@
+class MinStack:
+    def __init__(self):
+        self.stk1 = []
+        self.stk2 = [inf]
+
+    def push(self, val: int) -> None:
+        self.stk1.append(val)
+        self.stk2.append(min(val, self.stk2[-1]))
+
+    def pop(self) -> None:
+        self.stk1.pop()
+        self.stk2.pop()
+
+    def top(self) -> int:
+        return self.stk1[-1]
+
+    def getMin(self) -> int:
+        return self.stk2[-1]
