301f96728e89dc842c35e77183df7099f28f88b3
Shashank Singh <156345859+shashankexore@users.noreply.github.com>
2025-02-13 01:35:18 +0530

    0104-Maximum-Depth-Of-Binary-Tree.py

--- Python/0101-0200/0104-Maximum-Depth-Of-Binary-Tree.py
+++ Python/0101-0200/0104-Maximum-Depth-Of-Binary-Tree.py
@@ -4,3 +4,5 @@ class Solution:
             return 0
         l, r = self.maxDepth(root.left), self.maxDepth(root.right)
         return 1 + max(l, r)
+
+#https://leetcode.com/problems/maximum-depth-of-binary-tree/
