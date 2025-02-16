fe90198dbbfd872b5051654087d2e9c5a0080f43
Shashank Singh <156345859+shashankexore@users.noreply.github.com>
2025-02-17 04:28:50 +0530

    Create 0235.py

---
+++ Python/0201-0300/0235.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lowestCommonAncestor(
+        self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode'
+    ) -> 'TreeNode':
+        while 1:
+            if root.val < min(p.val, q.val):
+                root = root.right
+            elif root.val > max(p.val, q.val):
+                root = root.left
+            else:
+                return root
