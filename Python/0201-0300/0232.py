56726ba9ff0d5fb464d52376c320535361e1f20d
Shashank Singh <156345859+shashankexore@users.noreply.github.com>
2025-02-17 04:27:51 +0530

    0232.py

---
+++ Python/0201-0300/0232.py
@@ -0,0 +1,23 @@
+class MyQueue:
+    def __init__(self):
+        self.stk1 = []
+        self.stk2 = []
+
+    def push(self, x: int) -> None:
+        self.stk1.append(x)
+
+    def pop(self) -> int:
+        self.move()
+        return self.stk2.pop()
+
+    def peek(self) -> int:
+        self.move()
+        return self.stk2[-1]
+
+    def empty(self) -> bool:
+        return not self.stk1 and not self.stk2
+
+    def move(self):
+        if not self.stk2:
+            while self.stk1:
+                self.stk2.append(self.stk1.pop())
