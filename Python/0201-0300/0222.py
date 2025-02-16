760fd9fda828288cb95407e4f63548e1087ce3fc
Shashank Singh <156345859+shashankexore@users.noreply.github.com>
2025-02-16 17:18:45 +0530

    0222.py

---
+++ Python/0201-0300/0222.py
@@ -0,0 +1,5 @@
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
