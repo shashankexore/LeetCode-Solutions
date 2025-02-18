d6a4787e1f54a35bc6453d255337b2ab1061be72
Shashank Singh <156345859+shashankexore@users.noreply.github.com>
2025-02-18 23:59:58 +0530

    0250.py

---
+++ Python/0201-0300/0250.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countUnivalSubtrees(self, root: Optional[TreeNode]) -> int:
+        def dfs(root):
+            if root is None:
+                return True
+            l, r = dfs(root.left), dfs(root.right)
+            if not l or not r:
+                return False
+            a = root.val if root.left is None else root.left.val
+            b = root.val if root.right is None else root.right.val
+            if a == b == root.val:
+                nonlocal ans
+                ans += 1
+                return True
+            return False
+
+        ans = 0
+        dfs(root)
+        return ans
