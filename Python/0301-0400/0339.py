e522a95cb9886fee15213fab73aad102b7f47f4e
Shashank Singh <156345859+shashankexore@users.noreply.github.com>
2025-02-27 21:19:57 +0530

    Create 0339.py

---
+++ Python/0301-0400/0339.py
@@ -0,0 +1,12 @@
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        def dfs(nestedList, depth):
+            depth_sum = 0
+            for item in nestedList:
+                if item.isInteger():
+                    depth_sum += item.getInteger() * depth
+                else:
+                    depth_sum += dfs(item.getList(), depth + 1)
+            return depth_sum
+
+        return dfs(nestedList, 1)
