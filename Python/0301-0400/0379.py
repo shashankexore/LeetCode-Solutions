34f5094f7c3156bbcae12ddae895bca0749997f5
Shashank Singh <156345859+shashankexore@users.noreply.github.com>
2025-03-03 00:32:16 +0530

    0379.py

---
+++ Python/0301-0400/0379.py
@@ -0,0 +1,15 @@
+class PhoneDirectory:
+
+    def __init__(self, maxNumbers: int):
+        self.available = set(range(maxNumbers))
+
+    def get(self) -> int:
+        if not self.available:
+            return -1
+        return self.available.pop()
+
+    def check(self, number: int) -> bool:
+        return number in self.available
+
+    def release(self, number: int) -> None:
+        self.available.add(number)
