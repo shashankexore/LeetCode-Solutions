4d5e2f499d4f36418f6a1f7b0b7974fbfaccfaa9
Shashank Singh <156345859+shashankexore@users.noreply.github.com>
2025-03-05 17:21:33 +0530

    0398.py

---
+++ Python/0301-0400/0398.py
@@ -0,0 +1,13 @@
+class Solution:
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+
+    def pick(self, target: int) -> int:
+        n = ans = 0
+        for i, v in enumerate(self.nums):
+            if v == target:
+                n += 1
+                x = random.randint(1, n)
+                if x == n:
+                    ans = i
+        return ans
