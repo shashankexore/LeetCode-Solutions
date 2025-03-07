f0850917039172e08e69ab21ab5112b9d8e95dfd
Shashank Singh <156345859+shashankexore@users.noreply.github.com>
2025-03-08 01:14:21 +0530

    0430.py

---
+++ Python/0401-0500/0430.py
@@ -0,0 +1,19 @@
+class Solution:
+    def flatten(self, head: 'Node') -> 'Node':
+        def preorder(pre, cur):
+            if cur is None:
+                return pre
+            cur.prev = pre
+            pre.next = cur
+
+            t = cur.next
+            tail = preorder(cur, cur.child)
+            cur.child = None
+            return preorder(tail, t)
+
+        if head is None:
+            return None
+        dummy = Node(0, None, head, None)
+        preorder(dummy, head)
+        dummy.next.prev = None
+        return dummy.next
