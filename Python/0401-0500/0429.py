550d7c17a913663906f1a2814a64ac20548bd24b
Shashank Singh <156345859+shashankexore@users.noreply.github.com>
2025-03-08 01:14:01 +0530

    0429.py

---
+++ Python/0401-0500/0429.py
@@ -0,0 +1,14 @@
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        ans = []
+        if root is None:
+            return ans
+        q = deque([root])
+        while q:
+            t = []
+            for _ in range(len(q)):
+                root = q.popleft()
+                t.append(root.val)
+                q.extend(root.children)
+            ans.append(t)
+        return ans
