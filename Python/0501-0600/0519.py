18a33a61ffcb91dcd7c560ba7e7af71dc29c3bea
Shashank Singh <156345859+shashankexore@users.noreply.github.com>
2025-05-05 19:17:38 +0530

    0519.py

---
+++ Python/0501-0600/0519.py
@@ -0,0 +1,17 @@
+class Solution:
+    def __init__(self, m: int, n: int):
+        self.m = m
+        self.n = n
+        self.total = m * n
+        self.mp = {}
+
+    def flip(self) -> List[int]:
+        self.total -= 1
+        x = random.randint(0, self.total)
+        idx = self.mp.get(x, x)
+        self.mp[x] = self.mp.get(self.total, self.total)
+        return [idx // self.n, idx % self.n]
+
+    def reset(self) -> None:
+        self.total = self.m * self.n
+        self.mp.clear()
