7f25cd4081c3581f43c0e38da6f01cf678f4a38c
Shashank Singh <156345859+shashankexore@users.noreply.github.com>
2025-06-04 15:01:39 +0530

    0538.py

---
+++ Python/0501-0600/0538.py
@@ -0,0 +1,14 @@
+class Solution:
+    def convertBST(self, root: TreeNode) -> TreeNode:
+        def dfs(root):
+            nonlocal s
+            if root is None:
+                return
+            dfs(root.right)
+            s += root.val
+            root.val = s
+            dfs(root.left)
+
+        s = 0
+        dfs(root)
+        return root
